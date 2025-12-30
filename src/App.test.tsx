bf7bd6028b359fd2e21a68b10fcb42433a8278f2
Martin Metaksov <metaksov@gmail.com>
2025-12-31 00:52:27 +0100

    fix: unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,5 +3,5 @@ import App from "./App";
 
 test("renders Story Master", () => {
   render(<App />);
-  expect(screen.getByText(/story master/i)).toBeInTheDocument();
+  expect(screen.getAllByText(/story master/i).length).toBeGreaterThan(0);
 });
